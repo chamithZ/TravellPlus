8abbd4fcf6589a70c8882e02c6ae83bb878bb20c
it21156960 <chamith227@gmail.com>
2024-03-26 17:20:14 +0530

    contractServiceUpdated

---
+++ TravelPlus_frontend/travelPlus/src/app/Services/ContractService/contract.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { contract } from '../../Models/Contract';
+import { Response } from '../../Models/Response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContractService {
+  baseUrl="http://localhost:8080/api/v1"
+
+  constructor(private http:HttpClient) { }
+
+  httpOptions={
+    headers:new HttpHeaders({'Content-Type':'application/json'})
+  }
+  addContract(contract:contract){
+    return this.http.post<Response<contract>>(`${this.baseUrl}/contract`,contract,this.httpOptions)
+  }
+}
