94e820fd3497f99955311139a8410cc47b233e97
it21156960 <chamith227@gmail.com>
2024-04-04 09:31:34 +0530

    DTOupdated

--- TravelPlus_backend/travelPlus/src/main/java/com/travelPlus/v1/DTO/ReservationDTO.java
+++ TravelPlus_backend/travelPlus/src/main/java/com/travelPlus/v1/DTO/ReservationDTO.java
@@ -5,19 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReservationDTO {
     private long reservationId;
-    private String checkInDate;
-    private String checkOutDate;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
     private int childCount;
     private int adultCount;
-    boolean isFullPayment;
-    private long roomTypeId;
     private long userId;
     private ReservationRoomType reservationRoomType;
     private PaymentDTO paymentDTO;
+    private  boolean isFullPayment;
+    private Collection<RoomTypeReservationDTO> roomTypeReservation=new ArrayList<>();
+    private Collection<ReservationSupplementDTO> reservationSupplementDTOS=new ArrayList<>();
+    private Collection<ReservationOffersDTO> reservationOffersDTOS=new ArrayList<>();
 
 }
