b0d5305d9ec95bbd185f31d9cd578581c40801d9
it21156960 <chamith227@gmail.com>
2024-04-06 04:16:44 +0530

    repoUpdated

--- TravelPlus_backend/travelPlus/src/main/java/com/travelPlus/v1/Repo/RoomTypeRepo.java
+++ TravelPlus_backend/travelPlus/src/main/java/com/travelPlus/v1/Repo/RoomTypeRepo.java
@@ -2,9 +2,25 @@ package com.travelPlus.v1.Repo;
 
 import com.travelPlus.v1.Entity.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomTypeRepo extends JpaRepository<RoomType,Long> {
     List<RoomType> findByContract_contractId(long contractId);
+
+    // Modified query to fetch room types along with room type seasons
+    @Query("SELECT DISTINCT rt FROM RoomType rt " +
+            "JOIN FETCH rt.roomTypeSeasons rts " + // Fetch roomTypeSeasons collection
+            "JOIN FETCH rts.season s " +
+            "WHERE rt.contract.contractId = :contractId " +
+            "AND s.startDate <= :checkOutDate " +
+            "AND s.endDate >= :checkInDate")
+    List<RoomType> findRoomTypesByContractIdAndSeasonValidity(
+            @Param("contractId") long contractId,
+            @Param("checkInDate") LocalDate checkInDate,
+            @Param("checkOutDate") LocalDate checkOutDate
+    );
 }
