30889484a161b294decaa0a0992ed17ab0f68869
it21156960 <chamith227@gmail.com>
2024-04-10 02:13:18 +0530

    reservationOverviewAdded

---
+++ TravelPlus_frontend/travelPlus/src/app/Pages/reservation-overview/reservation-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-overview',
+  templateUrl: './reservation-overview.component.html',
+  styleUrl: './reservation-overview.component.css'
+})
+export class ReservationOverviewComponent {
+
+}
