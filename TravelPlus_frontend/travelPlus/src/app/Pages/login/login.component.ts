1d0bb067d4b9947e8768dd6b6784047033974010
it21156960 <chamith227@gmail.com>
2024-04-10 02:13:01 +0530

    loginAdded

---
+++ TravelPlus_frontend/travelPlus/src/app/Pages/login/login.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../Services/AuthService/auth-service.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from '../../Models/User';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  userId!: number;
+
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const credentials = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: response => {
+        const token = response.token;
+        localStorage.setItem('token', token);
+        console.log(token)
+
+        
+      },
+      error: err => {
+        // Handle login error
+        console.error('Login error:', err);
+      }
+    });
+
+    this.authService.getUserByEmail(credentials.email).subscribe({
+      next: response => {
+        const user: User = response.content;
+        this.userId = user.userId;
+        localStorage.setItem('userId', this.userId.toString());
+        console.log(this.userId)
+      },
+      error: err => {
+        // Handle get user error
+        console.error('Get user error:', err);
+      }
+    });
+
+
+
+  }
+}
